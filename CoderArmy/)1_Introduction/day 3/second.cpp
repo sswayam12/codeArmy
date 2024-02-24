0e50e20b94481fddae481049f5281bcab332e8af
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-24 00:19:01 +0000

    introduction

---
+++ CoderArmy/)1_Introduction/day 3/second.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // variable declaration
+    int a;
+    a = 4;
+    cout<<a<<endl;
+
+    char name ='a';
+    cout<<name<<endl;
+
+    float f = 2.45;
+    cout<<f<<endl;
+
+    double d = 3.5574883735;
+    cout<<d<<endl;
+
+    bool flag = true;
+    cout<<flag<<endl;
+
+    return 0;
+}
