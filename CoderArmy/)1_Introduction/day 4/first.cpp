8f8d023e24995f9d80761b98ffdc4d2adb3c20ca
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-24 00:21:19 +0000

    Day 4

---
+++ CoderArmy/)1_Introduction/day 4/first.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // int a ,b;
+    // cin>>a>>b;
+
+    // cout<<a+b;
+
+    int a = 10;
+    char c = 'b';
+    a = c;
+
+    cout<<a;
+
+   return 0; 
+}
---
+++ CoderArmy/)1_Introduction/day 4/second.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // int package;
+    // cin>>package;
+    // if(package > 10){
+        // cout<<"Accepted";
+    // }
+    // else{
+        // cout<<"Rejected";
+    // }*/
+
+    // int marks;
+    // cin>>marks;
+
+    // if(marks> 33){
+    //     cout<<"pass";
+    // }
+    // else{
+    //     cout<<"fail";
+    // }~
+
+
+    int a,b;
+    cin>>a>>b;
+    if(a>b){
+        cout<<"a is greater than b";
+    }
+    else{
+        cout<<"b is greater";
+    }
+
+
+}
