7451cf9dcac1c5924b550e498c507165d389fe25
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-08-04 19:29:21 +0000

    push

---
+++ CoderArmy/Graphs/Adjcent Matrix/direcWeighted.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int vertex,edges;
+    cin>>vertex>>edges;
+
+    vector<vector<int> >adjMat(vertex, vector<int>(vertex,0));
+    
+    
+    int u,v,weight;
+    for(int i = 0;i<edges;i++){
+        
+        cin>>u>>v>>weight;
+
+        adjMat[u][v] = weight;
+
+    }
+    for(int i = 0;i<vertex;i++){
+        for(int j = 0; j< vertex;j++){
+            cout<<adjMat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
