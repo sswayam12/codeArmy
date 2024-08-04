7f63d718fdf0c4cdb3b5051e8ffac7e94241ef41
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-08-04 19:59:36 +0000

    push

---
+++ CoderArmy/Graphs/Adjacent list/undirecWeighted.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int vertex,edges;
+    cin>>vertex>>edges;
+
+    vector<pair<int,int>>adjList[vertex + 1];
+    
+    
+    int u,v,weight;
+    for(int i = 0;i<edges;i++){
+        
+        cin>>u>>v>>weight;
+
+        adjList[u].push_back(make_pair(v,weight));
+        adjList[v].push_back(make_pair(u,weight));
+
+    }
+    for(int i = 0;i<vertex;i++){
+        cout<<i<<"->";
+        for(int j = 0; j < adjList[i].size();j++){
+            cout<<adjList[i][j].first<<" "<<adjList[i][j].second<<" ";
+        }
+        cout<<endl;
+    }
+}
